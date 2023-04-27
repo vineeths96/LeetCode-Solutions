8a5d6534c697a798e05ee0b442135b64eafa975d
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-04-27 07:46:01 +0530

    Time: 6 ms (69.79%) | Memory: 7.9 MB (22.05%) - LeetSync

--- reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -5,14 +5,12 @@ public:
         int end = s.size() - 1;
 
         std::unordered_set<char> vowels({'a', 'e', 'i', 'o', 'u'});
-        while (start <= end) {
-            while (start < s.size() && vowels.count(std::tolower(s[start])) == 0)
+        while (start < end) {
+            while (start < end && vowels.count(std::tolower(s[start])) == 0)
                 start++;
             
-            while (end >=0 && vowels.count(std::tolower(s[end])) == 0)
+            while (start < end && vowels.count(std::tolower(s[end])) == 0)
                 end--;
-
-            if (start > end) break;
 
             std::swap(s[start], s[end]);
             start++;
