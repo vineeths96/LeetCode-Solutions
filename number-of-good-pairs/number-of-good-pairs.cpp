0a3139c5a1d5b9f486f771c676916d0ebd672d71
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-04-29 10:28:25 +0530

    Time: 2 ms (50.69%) | Memory: 7.2 MB (61.59%) - LeetSync

--- number-of-good-pairs/number-of-good-pairs.cpp
+++ number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        std::unordered_map<int, std::vector<int>> indexTracker;
+        std::unordered_map<int, int> indexTracker;
 
         for (int i = 0; i < nums.size(); i++) 
-            indexTracker[nums[i]].push_back(i);
+            indexTracker[nums[i]]++;
 
         int count = 0;
         for (const auto& kv : indexTracker) {
-            int numIndices = kv.second.size();
+            int numIndices = kv.second;
             count += numIndices * (numIndices - 1) / 2;
         }
 
