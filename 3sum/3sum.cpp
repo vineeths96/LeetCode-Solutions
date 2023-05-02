32dab22f0b21761707752b329cfdf4fec70912f6
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-05-02 09:13:21 +0530

    Time: 115 ms (94.54%) | Memory: 106.1 MB (31.49%) - LeetSync

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void searchPair(std::vector<int> nums, int targetSum, int index, std::vector<std::vector<int>> &triplets) {
+    void searchPair(std::vector<int> nums, int index, std::vector<std::vector<int>> &triplets) {
         int start = index + 1;
         int end = nums.size() - 1;
+        int targetSum = -nums[index];
 
-        while(start < end) {
+        while (start < end) {
             int currentSum = nums[start] + nums[end];
-            if (currentSum == targetSum) {
+            if (currentSum > targetSum)
+                end--;
+            else if (currentSum < targetSum)
+                start++;
+            else {
                 triplets.push_back({nums[index], nums[start], nums[end]});
                 start++;
                 end--;
 
-                while (start < end && nums[start] == nums[start-1])
+                while (start < end && nums[start] == nums[start-1]) 
                     start++;
-
+                
                 while (start < end && nums[end] == nums[end+1])
                     end--;
-            } else if (currentSum > targetSum)
-                end--;
-            else
-                start++;
+            }
         }
     }
 
@@ -27,13 +29,11 @@ public:
         std::vector<std::vector<int>> triplets;
         std::sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size(); i++) {
-            while (i > 0 && i < nums.size() && nums[i] == nums[i-1])
-                i++;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
 
-            if (i == nums.size()) break;
-
-            searchPair(nums, -nums[i], i, triplets);
+            searchPair(nums, i, triplets);
         }
 
         return triplets;
