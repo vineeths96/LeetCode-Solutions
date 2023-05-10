142c60f957025b6458189d61793159cefd0e9c6f
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-05-10 08:39:07 +0530

    Time: 4 ms (37.78%) | Memory: 6.1 MB (91.96%) - LeetSync

--- backspace-string-compare/backspace-string-compare.cpp
+++ backspace-string-compare/backspace-string-compare.cpp
@@ -3,21 +3,19 @@ public:
     bool backspaceCompare(string s, string t) {
         std::string S, T;
 
-        for (const char& ch : s) {
+        for (const char &ch : s) {
             if (ch == '#') {
                 if (S.size())
-                    S.erase(S.size() - 1);
-            }
-            else
+                    S.erase(S.length() - 1);
+            } else
                 S.push_back(ch);
         }
 
-        for (const char& ch : t) {
+        for (const char &ch : t) {
             if (ch == '#') {
                 if (T.size())
-                    T.erase(T.size() - 1);
-            }
-            else
+                    T.erase(T.length() - 1);
+            } else
                 T.push_back(ch);
         }
 
