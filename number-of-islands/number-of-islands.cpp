1032a8ffce1fea51f116505e1510979f9a1f46d1
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-05-13 08:57:15 +0530

    Time: 42 ms (43.65%) | Memory: 17.8 MB (29.59%) - LeetSync

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        std::vector<std::vector<bool>> visited(grid.size(), std::vector<bool>(grid[0].size(), false));
         int islandCount = 0;
 
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
-                if (visited[i][j])
-                    continue;
-
                 if (grid[i][j] == '0')
                     continue;
 
                 std::stack<std::pair<int, int>> dfsStack;
                 dfsStack.push(std::make_pair(i, j));
-                visited[i][j] = true;
+                grid[i][j] = '0';
 
                 while (!dfsStack.empty()) {
                     auto top = dfsStack.top();
@@ -23,24 +19,24 @@ public:
                     int x = top.first;
                     int y = top.second;
 
-                    if (x > 0 && grid[x-1][y] == '1' && visited[x-1][y] == false) {
+                    if (x > 0 && grid[x-1][y] == '1') {
                         dfsStack.push(std::make_pair(x - 1, y));
-                        visited[x-1][y] = true;
+                        grid[x-1][y] = '0';
                     }
 
-                    if (x < grid.size() - 1 && grid[x+1][y] == '1' && visited[x+1][y] == false) {
+                    if (x < grid.size() - 1 && grid[x+1][y] == '1') {
                         dfsStack.push(std::make_pair(x + 1, y));
-                        visited[x+1][y] = true;
+                        grid[x+1][y] = '0';
                     }
 
-                    if (y > 0 && grid[x][y-1] == '1' && visited[x][y-1] == false) {
+                    if (y > 0 && grid[x][y-1] == '1') {
                         dfsStack.push(std::make_pair(x, y - 1));
-                        visited[x][y-1] = true;
+                        grid[x][y-1] = '0';
                     }
 
-                    if (y < grid[0].size() - 1 && grid[x][y+1] == '1' && visited[x][y+1] == false) {
+                    if (y < grid[0].size() - 1 && grid[x][y+1] == '1') {
                         dfsStack.push(std::make_pair(x, y + 1));
-                        visited[x][y+1] = true;
+                        grid[x][y+1] = '0';
                     }
                 }
 
