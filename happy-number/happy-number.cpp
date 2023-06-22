6f75802170b4bf8ac99943f0d5582213f5fae7ae
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-06-22 22:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (71.96%) - LeetSync

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -1,19 +1,26 @@
 class Solution {
+private:
+    int sumOfSquares(int n) {
+        int sum = 0;
+
+        while (n) {
+            sum += std::pow(n % 10, 2);
+            n = n / 10;
+        }
+
+        return sum;
+    }
+
 public:
     bool isHappy(int n) {
-        std::set<int> numbers;
+        int slowPointer = sumOfSquares(n);
+        int fastPointer = sumOfSquares(sumOfSquares(n));
 
-        do {
-            int sumOfSquares = 0;
-            
-            while (n) {
-                sumOfSquares += std::pow(n % 10, 2);
-                n = n / 10;
-            }
+        while (fastPointer != slowPointer) {
+            slowPointer = sumOfSquares(slowPointer);
+            fastPointer = sumOfSquares(sumOfSquares(fastPointer));
+        }
 
-            n = sumOfSquares;
-        } while (numbers.insert(n).second);
-
-        return n == 1;
+        return fastPointer == 1;
     }
 };
