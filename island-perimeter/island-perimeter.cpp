85fb0a35b2bb071d6f08e8dab45f96f0efebef76
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-06-22 07:24:47 +0530

    Time: 163 ms (12.90%) | Memory: 101.8 MB (10.19%) - LeetSync

--- island-perimeter/island-perimeter.cpp
+++ island-perimeter/island-perimeter.cpp
@@ -1,19 +1,50 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int columns = grid[0].size();
+        std::vector<std::vector<bool>> visited(rows, std::vector<bool>(columns, false));
+
         int perimeter = 0;
-
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 if (grid[i][j] == 0)
-                    continue;
+                    continue;    
 
-                perimeter += 4;
+                if (visited[i][j])
+                    continue;    
 
-                if (i > 0 && grid[i-1][j] == 1) perimeter--;
-                if (i < grid.size() - 1 && grid[i+1][j] == 1) perimeter--;
-                if (j > 0 && grid[i][j-1] == 1) perimeter--;
-                if (j < grid[0].size() - 1 && grid[i][j+1] == 1) perimeter--;
+                std::stack<std::pair<int, int>> dfsStack;
+                dfsStack.push(std::make_pair(i, j));
+                visited[i][j] = true;
+
+                while (!dfsStack.empty()) {
+                    auto top = dfsStack.top();
+                    dfsStack.pop();
+
+                    int x = top.first;
+                    int y = top.second;
+
+                    int dirx[] = {-1, 0, 1, 0};
+                    int diry[] = {0, -1, 0, 1};
+
+                    for (int k = 0; k < 4; k++) {
+                        int xdash = x + dirx[k];
+                        int ydash = y + diry[k];
+
+                        if (xdash < 0 || xdash >= grid.size() || y < 0 || ydash >= grid[0].size()) {
+                            perimeter++;
+                            continue;
+                        }
+                        
+                        if (grid[xdash][ydash] == 0)
+                            perimeter++;
+                        else if (!visited[xdash][ydash]) {
+                            dfsStack.push(std::make_pair(xdash, ydash));
+                            visited[xdash][ydash] = true;
+                        }
+                    }
+                }
             }
         }
 
