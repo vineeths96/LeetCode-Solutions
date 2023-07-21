66b226540a11ed3ff05ee12b99288ed036291fa1
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-07-21 11:32:51 +0530

    Time: 9 ms (80.55%) | Memory: 6.8 MB (98.38%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,38 +1,22 @@
 class Solution {
-private:
-    bool subStringWithSameLetter(std::string str, int windowSize, int k) {
-        int maxFrequency = std::numeric_limits<int>::min();
-        std::unordered_map<char, int> charFreqMap;
-
-        for (int windowEnd = 0; windowEnd < str.size(); windowEnd++) {
-            charFreqMap[str[windowEnd]]++;
-
-            if (windowEnd >= windowSize)
-                charFreqMap[str[windowEnd - windowSize]]--;
-
-            maxFrequency = std::max(maxFrequency, charFreqMap[str[windowEnd]]);
-
-            if (windowSize - maxFrequency <= k)
-                return true;
-        } 
-
-        return false;
-    }
-
 public:
     int characterReplacement(string s, int k) {
-        int left = 1;
-        int right = s.size() + 1;
+        std::array<int, 26> freqVector{0};
+        int maxFrequency =  0;
+        int subStringLength = std::numeric_limits<int>::min();
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        for (int windowStart = 0, windowEnd = 0; windowEnd < s.size(); windowEnd++) {
+            freqVector[s[windowEnd] - 'A']++;
+            maxFrequency = std::max(maxFrequency, freqVector[s[windowEnd] - 'A']);
 
-            if (subStringWithSameLetter(s, mid, k)) 
-                left = mid + 1;
-            else
-                right = mid;
+            if (windowEnd - windowStart + 1 - maxFrequency > k) {
+                freqVector[s[windowStart] - 'A']--;
+                windowStart++;
+            }
+
+            subStringLength = std::max(subStringLength, windowEnd - windowStart + 1);
         }
 
-        return left - 1;
+        return subStringLength;
     }
 };
