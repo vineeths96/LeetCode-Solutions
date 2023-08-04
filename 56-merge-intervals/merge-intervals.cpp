1f76bc24f0f6dce0e45d46431a5ceca185510548
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-04 08:23:13 +0530

    Time: 155 ms (8.42%) | Memory: 48.1 MB (5.05%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    static bool compare(std::vector<int> vecA, std::vector<int> vecB) {
+        return vecA[0] < vecB[0] || vecA[0] == vecB[0] && vecA[1] < vecB[1];
+    }
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::vector<std::vector<int>> mergedIntervals;
+        std::sort(intervals.begin(), intervals.end(), compare);
+
+        for (int ind = 0; ind < intervals.size(); ind++) {
+            std::vector<int> &currentInterval = intervals[ind];
+            if (mergedIntervals.empty() || currentInterval[0] >  mergedIntervals.back()[1])
+                mergedIntervals.push_back(currentInterval);
+            else {
+                if (currentInterval[1] >  mergedIntervals.back()[1])
+                     mergedIntervals.back()[1] = currentInterval[1];
+            }
+        }
+
+        return mergedIntervals;
+    }
+};
