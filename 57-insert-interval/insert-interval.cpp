7af166573d7ac6b9b846ca3dbbf9d8f2cd72a485
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-04 10:21:47 +0530

    Time: 9 ms (95.62%) | Memory: 17.1 MB (25.49%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    void insertInterval(std::vector<std::vector<int>> &insertedIntervals, std::vector<int> &newInterval) {
+        if (insertedIntervals.empty() || newInterval[0] > insertedIntervals.back()[1])
+            insertedIntervals.push_back(newInterval);
+        else {
+            auto &lastInterval = insertedIntervals.back();
+            if (newInterval[1] > lastInterval[1])
+                lastInterval[1] = newInterval[1];
+        }
+    }
+
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int idxToInsert = 0;
+        while (idxToInsert < intervals.size() && intervals[idxToInsert][0] < newInterval[0])
+            idxToInsert++;
+
+        std::vector<std::vector<int>> insertedIntervals;
+        for (int current = 0; current < idxToInsert; current++)
+            insertedIntervals.push_back(intervals[current]);
+
+        insertInterval(insertedIntervals, newInterval);
+        for (int current = idxToInsert; current < intervals.size(); current++)
+            insertInterval(insertedIntervals, intervals[current]);
+
+        return insertedIntervals;        
+    }
+};
