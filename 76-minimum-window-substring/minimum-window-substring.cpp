ecde0c798df4e7912ed21f60568f65c1e64add87
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-08 06:30:27 +0530

    Time: 24 ms (61.77%) | Memory: 8.2 MB (23.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -6,35 +6,28 @@ public:
             charFreqMap[ch]++;
 
         std::vector<int> minWindow{std::numeric_limits<int>::max(), 0, 0};
-        int windowStart = 0;
-        int windowEnd = 0;
         int matched = 0;
         std::unordered_map<char, int> windowCharFreqMap;
-        while (windowEnd < s.size()) {
-            windowCharFreqMap[s[windowEnd]]++;
+        for (int windowStart = 0, windowEnd = 0; windowEnd < s.size(); windowEnd++) {
+            char windowEndChar = s[windowEnd];
+            windowCharFreqMap[windowEndChar]++;
             
-            if (charFreqMap.count(s[windowEnd]) != 0 && windowCharFreqMap[s[windowEnd]] == charFreqMap[s[windowEnd]])
+            if (charFreqMap.count(windowEndChar) != 0 && windowCharFreqMap[windowEndChar] == charFreqMap[windowEndChar])
                 matched++;
 
-            if (matched == charFreqMap.size()) {
-                while (windowStart <= windowEnd) {
-                    if (matched == charFreqMap.size() && windowEnd - windowStart + 1 < minWindow[0]) {
-                        minWindow[0] = windowEnd - windowStart + 1;
-                        minWindow[1] = windowStart;
-                        minWindow[2] = windowEnd;
-                    }
-
-                    char windowStartChar = s[windowStart];
-                    windowCharFreqMap[windowStartChar]--;
-                    windowStart++;
-                    if (charFreqMap.count(windowStartChar) != 0 && windowCharFreqMap[windowStartChar] < charFreqMap[windowStartChar]) {
-                        matched--;
-                        break;
-                    }
+            while (matched == charFreqMap.size()) {
+                if (windowEnd - windowStart + 1 < minWindow[0]) {
+                    minWindow[0] = windowEnd - windowStart + 1;
+                    minWindow[1] = windowStart;
+                    minWindow[2] = windowEnd;
                 }
-            }
 
-            windowEnd++;
+                char windowStartChar = s[windowStart];
+                windowCharFreqMap[windowStartChar]--;
+                windowStart++;
+                if (charFreqMap.count(windowStartChar) != 0 && windowCharFreqMap[windowStartChar] < charFreqMap[windowStartChar])
+                    matched--;
+            }
         }
 
         return minWindow[0] == std::numeric_limits<int>::max() ? "" : s.substr(minWindow[1], minWindow[0]);
