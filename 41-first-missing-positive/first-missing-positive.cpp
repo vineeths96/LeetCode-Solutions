1397e4ae722c6097d0e16682b18455d051b86f5d
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-14 06:49:57 +0530

    Time: 43 ms (95.77%) | Memory: 41.2 MB (60.14%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -12,16 +12,16 @@ public:
                 start++;
         }
 
-        start = 0;
-        while (start < end) {
-            if (nums[start] > 0 && nums[start] < nums.size() && nums[start] != nums[nums[start] - 1])
-                std::swap(nums[start], nums[nums[start] - 1]);
-            else
-                start++;
+        for (int start = 0; start < end; start++) {
+            if (nums[start] - 1 < end) {
+                int newIndex = std::abs(nums[start]) - 1;
+                if (nums[newIndex] > 0)
+                    nums[newIndex] *= -1;
+            }
         }
 
         for (int i = 0; i < end; i++) {
-            if (nums[i] != i + 1)
+            if (nums[i] > 0)
                 return i + 1;
         }
 
