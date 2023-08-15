f5764a24fffc112c9d489cc1a32131465f4163a8
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-15 08:08:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (5.53%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        std::unordered_map<int, int> indexTracker;
+        
+        for (int i = 0; i < nums.size(); i++) 
+            indexTracker[nums[i]]++;
+
+        int count = 0;
+        for (const auto& kv : indexTracker) {
+            int numIndices = kv.second;
+            count += numIndices * (numIndices - 1) / 2;
+        }
+
+        return count;
+    }
+};
