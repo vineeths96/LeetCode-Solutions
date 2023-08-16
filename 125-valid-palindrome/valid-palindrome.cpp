54a1f22c411c9b95951f492368fb5e9eaf8a0494
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-16 20:44:35 +0530

    Time: 2 ms (91.76%) | Memory: 7.4 MB (64.82%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int start = 0;
+       int end = s.size() - 1;
+       
+       while(start < end) {
+           while (start < end && std::isalnum(s[start]) == false)
+                start++;
+
+            while(start < end && std::isalnum(s[end]) == false)
+                end--;
+            
+            if (std::tolower(s[start]) != std::tolower(s[end]))
+                return false;
+
+            start++;
+            end--;
+       }
+
+       return true; 
+    }
+};
