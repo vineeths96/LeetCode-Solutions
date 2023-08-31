0b72706510a9c642f4892d455c4eb81e36e49c41
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-08-31 07:18:06 +0530

    Time: 9 ms (33.96%) | Memory: 10.9 MB (69.18%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -2,14 +2,14 @@ class Solution {
     std::vector<std::string> permutations;
 
     void recursionHelper(std::string str, int currentIndex) {
-        while (str[currentIndex] >= '0' && str[currentIndex] <= '9') currentIndex++;
+        while (std::isdigit(str[currentIndex])) currentIndex++;
 
         if (currentIndex == str.size()) {
             permutations.push_back(str);
             return;
         }
 
-        int delta = str[currentIndex] >= 'a' && str[currentIndex] <= 'z' ? 'A' - 'a' : 'a' - 'A';
+        int delta = std::islower(str[currentIndex]) ? 'A' - 'a' : 'a' - 'A';
         str[currentIndex] += delta;
         recursionHelper(str, currentIndex + 1);
         str[currentIndex] -= delta;
