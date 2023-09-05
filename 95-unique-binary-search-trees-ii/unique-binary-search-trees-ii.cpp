ff02c6155f1fc04dd5313f27e2987b429b0356bd
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-05 19:16:51 +0530

    Time: 19 ms (17.10%) | Memory: 15.8 MB (52.63%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    std::vector<TreeNode*> generateTreesHelper(int start, int end) {
+        if (start > end)
+            return {nullptr};
+
+        if (start == end) {
+            TreeNode *root = new TreeNode(start);
+            return {root};
+        }
+        
+        std::vector<TreeNode*> roots;
+        for (int i = start; i <= end; i++) {
+            std::vector<TreeNode*> leftTrees = generateTreesHelper(start, i - 1);
+            std::vector<TreeNode*> rightTrees = generateTreesHelper(i + 1, end);
+
+            for (const auto &left : leftTrees) {
+                for (const auto &right : rightTrees) {
+                    TreeNode *root = new TreeNode(i, left, right);
+                    roots.push_back(root);
+                }
+            }
+        }
+
+        return roots;
+    }
+
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        return generateTreesHelper(1, n);
+    }
+};
