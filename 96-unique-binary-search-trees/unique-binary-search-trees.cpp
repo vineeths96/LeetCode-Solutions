2952bafefdd3dd589a7c46947bc1d61734bbef4e
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-06 07:28:20 +0530

    Time: 2 ms (58.20%) | Memory: 6.1 MB (14.15%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,20 +1,14 @@
 class Solution {
-    std::unordered_map<int, int> dpMap;
-
 public:
-    int numTrees(int n) {        
-        if (n == 0 || n == 1) 
-            return  1;
+    int numTrees(int n) {
+        std::vector<int> trees(n + 1, 0);
+        trees[0] = 1;
+        trees[1] = 1;
 
-        if (dpMap.find(n) != dpMap.end())
-            return dpMap[n];
+        for (int i = 2; i <= n; i++)
+            for (int j = 1; j <=i; j++)
+                trees[i] += trees[j - 1] * trees[i - j];
 
-        int trees = 0;
-        for (int i = 1; i <= n; i++)
-            trees += numTrees(i - 1) * numTrees(n - i);
-
-        dpMap[n] = trees;
-        
-        return trees;
+        return trees[n];
     }
 };
