3b4d5cc4c29c49de3ebbc57cb175bfa411b03445
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-07 19:39:52 +0530

    Time: 5 ms (70.96%) | Memory: 13.7 MB (35.63%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,28 +3,24 @@ class Solution {
         int left = 0;
         int right = nums.size() - 1;
 
-        int index = startingSearch ? nums.size() : -1;
+        int index = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) 
-                index = startingSearch ? std::min(index, mid) : std::max(index, mid);
-            
-            if (startingSearch) {
-                if (nums[mid] >= target)
-                    right = mid - 1;
-                else
-                    left = mid + 1;
-            }
+            if (target < nums[mid])
+                right = mid - 1;
+            else if (nums[mid] < target)
+                left = mid + 1;
             else {
-                if (nums[mid] <= target)
-                    left = mid + 1;
-                else
+                index = mid;
+                if (startingSearch)
                     right = mid - 1;
+                else
+                    left = mid + 1;
             }
         }
 
-        return index == nums.size() ? -1 : index;
+        return index;
     }
 
 public:
