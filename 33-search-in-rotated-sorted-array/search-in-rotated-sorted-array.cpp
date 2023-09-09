a9bc984c6f6c399a7c8dc1b675d6a161277ac93e
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-09 07:06:25 +0530

    Time: 3 ms (67.03%) | Memory: 11.1 MB (62.33%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,37 +1,27 @@
 class Solution {
-    int binarySearch(std::vector<int> nums, int target, int start, int end) {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (nums[mid] > target)
-                end = mid - 1;
-            else if (nums[mid] < target)
-                start = mid + 1;
-            else
+            if (nums[mid] == target) 
                 return mid;
+
+            if (nums.front() <= nums[mid]) {
+                if (nums.front() <= target && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums.back())
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
         }
-
-        return -1;
-    }
-
-public:
-    int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums.front() <= nums[mid])
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-
-        int leftSearch = binarySearch(nums, target, 0, left);
-        if (leftSearch != -1) return leftSearch;
-        int rightSearch = binarySearch(nums, target, left, nums.size() - 1);
-        if (rightSearch != -1) return rightSearch;
 
         return -1;
     }
