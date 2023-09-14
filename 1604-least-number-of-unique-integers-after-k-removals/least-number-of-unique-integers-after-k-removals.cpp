a98097fd52d3e516589153abcd3011c4dfc2ff6a
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-14 12:26:09 +0530

    Time: 152 ms (55.81%) | Memory: 65.4 MB (17.65%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    struct Compare {
+        bool operator()(std::pair<int, int> X, std::pair<int, int> Y) {
+            return X.second > Y.second;
+        }
+    };
+
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        std::unordered_map<int, int> freqMap;
+        for (const int &num : arr)
+            freqMap[num]++;
+
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, Compare> minHeap;
+        for (const auto kvPair : freqMap)
+            minHeap.push(kvPair);
+
+        while (!minHeap.empty() && k) {
+            if (k < minHeap.top().second)
+                break;
+            
+            k -= minHeap.top().second;
+            minHeap.pop();
+        }
+
+        return minHeap.size();
+    }
+};
