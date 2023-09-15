1d727532f7dd6df1c69abebefd77d27c48285975
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-15 18:02:42 +0530

    Time: 1553 ms (5.01%) | Memory: 57.2 MB (93.49%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,35 +1,31 @@
 class Solution {
-    static int distance(const std::vector<int> &point) {
+    int distance(const std::vector<int> &point) {
         return point[0] * point[0] + point[1] * point[1];
     }
 
-    struct Compare {
-        bool operator()(const std::vector<int> &X, const std::vector<int> &Y) {
-            return distance(X) < distance(Y);
+    int partition(std::vector<std::vector<int>> &points, int start, int end) {
+        int pivot = start;
+        for (int i = start; i < end; i++) {
+            if (distance(points[i]) <= distance(points[end]))
+                std::swap(points[i], points[pivot++]);
         }
-    };
+        std::swap(points[end], points[pivot]);
+
+        return pivot;
+    }
+
+    void quickSelect(std::vector<std::vector<int>> &points, int start, int end, int k) {
+        if (start >= end) return;
+        
+        int pivot = partition(points, start, end);
+        if (k == pivot) return;
+        else if (k < pivot) quickSelect(points, start, pivot - 1, k);
+        else quickSelect(points, pivot + 1, end, k);
+    }
 
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, Compare> maxHeap;
-
-        for (int i = 0; i < points.size(); i++) {
-            if (i < k) {
-                maxHeap.push(points[i]);
-                continue;
-            }
-
-            if (distance(points[i]) < distance(maxHeap.top())) {
-                maxHeap.pop();
-                maxHeap.push(points[i]);
-            }
-        }
-
-        std::vector<std::vector<int>> kClosePoints;
-        while (!maxHeap.empty()) {
-            kClosePoints.push_back(maxHeap.top());
-            maxHeap.pop();
-        }
-        return kClosePoints;
+        quickSelect(points, 0, points.size() - 1, k);
+        return std::vector<std::vector<int>>(points.begin(), points.begin() + k);
     }
 };
