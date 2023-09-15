e04795cca33fb156bb6178c28b73517e497d16cf
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-15 11:20:32 +0530

    Time: 155 ms (8.42%) | Memory: 52.2 MB (7.56%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    struct Compare {
+        bool operator()(const std::pair<char, int> &X, const std::pair<char, int> &Y) {
+            return X.second < Y.second;
+        }
+    };
+
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        std::unordered_map<char, int> charFreqMap;
+        for (const char &ch : tasks)
+            charFreqMap[ch]++;
+
+        std::priority_queue<std::pair<char, int>, std::vector<std::pair<char, int>>, Compare> maxHeap;
+        for (const auto &kvPair : charFreqMap)
+            maxHeap.push(kvPair);
+
+        std::queue<std::pair<char, int>> previousEntries;
+        int timeUnits = 0;
+        while (!maxHeap.empty()) {
+            auto top = maxHeap.top();
+            maxHeap.pop();
+            timeUnits++;
+            top.second--;
+
+            previousEntries.push(top);
+            if (previousEntries.size() > n) {
+                auto front = previousEntries.front();
+                previousEntries.pop();
+
+                if (front.second > 0) 
+                    maxHeap.push(front);
+            }
+
+            while (maxHeap.empty() && !previousEntries.empty() && previousEntries.front().second <= 0)
+                previousEntries.pop();
+
+            if (maxHeap.empty() && !previousEntries.empty() && previousEntries.front().second > 0)
+                maxHeap.push({' ', 0});
+        }
+
+        return timeUnits;
+    }
+};
