2370111e7896c8c50efdc110d5cecad1af7169e3
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-18 18:59:25 +0530

    Time: 42 ms (81.89%) | Memory: 16 MB (71.24%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    struct Compare {
+        bool operator()(const std::pair<int, std::pair<int, int>> &A, const std::pair<int, std::pair<int, int>> &B) {
+            return A.first > B.first;
+        }
+    };
+
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, Compare> minHeap;
+        int maxValuePushed = std::numeric_limits<int>::min();
+        for (int i = 0; i < nums.size(); i++) {
+            if (!nums[i].empty()) {
+                minHeap.push({nums[i][0], {i, 0}});
+                maxValuePushed = std::max(maxValuePushed, nums[i][0]);
+            }
+        }
+
+        std::vector<int> range({minHeap.top().first, maxValuePushed});
+        while (!minHeap.empty()) {
+            auto top = minHeap.top();
+            minHeap.pop();
+
+            if (minHeap.size() == nums.size() - 1 && maxValuePushed - top.first < range[1] - range[0])
+                range = {top.first, maxValuePushed};
+
+            top.second.second++;
+            if (top.second.second < nums[top.second.first].size()) {
+                minHeap.push({nums[top.second.first][top.second.second], {top.second.first, top.second.second}});
+                maxValuePushed = std::max(maxValuePushed, nums[top.second.first][top.second.second]);
+            }
+        }
+
+        return range;
+    }
+};
