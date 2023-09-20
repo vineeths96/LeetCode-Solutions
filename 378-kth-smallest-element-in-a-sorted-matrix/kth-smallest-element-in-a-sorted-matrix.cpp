fdcb0a2f1842c61b7694c8b116dc9ab481d11741
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-20 08:46:03 +0530

    Time: 22 ms (80.43%) | Memory: 13.3 MB (66.07%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,29 +1,42 @@
 class Solution {
-    struct Compare {
-        bool operator()(const std::pair<int, std::pair<int, int>> &A, const std::pair<int, std::pair<int, int>> &B) {
-            return A.first > B.first;
+    std::vector<int> countElementsLessThanEquals(std::vector<std::vector<int>> &matrix, int mid) {
+        int i = matrix.size() - 1;
+        int j = 0;
+
+        std::vector<int> countValueVec({0, std::numeric_limits<int>::min(), std::numeric_limits<int>::max()});
+        while (i >= 0 && j < matrix[0].size()) {
+            if (matrix[i][j] <= mid) {
+                countValueVec[0] += i + 1;
+                countValueVec[1] = std::max(countValueVec[1], matrix[i][j]);
+                j++;
+            }
+            else {
+                countValueVec[2] = std::min(countValueVec[2], matrix[i][j]);
+                i--;
+            }
         }
-    };
+
+        return countValueVec;
+    }
 
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, Compare> minHeap;
-        for (int i = 0; i < matrix.size(); i++)
-            if (!matrix[i].empty())
-                minHeap.push({matrix[i][0], {i, 0}});
-        
-        int index = 0;
-        while (!minHeap.empty()) {
-            auto top = minHeap.top();
-            minHeap.pop();
+        int left = matrix[0][0];
+        int right = matrix[matrix.size() - 1][matrix[0].size() - 1];
 
-            if (++index == k) return top.first;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            auto countValueVec = countElementsLessThanEquals(matrix, mid);
             
-            top.second.second++;
-            if (top.second.second < matrix[top.second.first].size())
-                minHeap.push({matrix[top.second.first][top.second.second], {top.second.first, top.second.second}}); 
+            if (countValueVec[0] == k)
+                return countValueVec[1];
+            else if (countValueVec[0] < k)
+                left = countValueVec[2];
+            else 
+                right = countValueVec[1];
         }
 
-        return -1;
+        return left;
     }
 };
