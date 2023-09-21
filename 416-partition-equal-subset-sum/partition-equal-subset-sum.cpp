96e64337ec6e321e5b2e040062ab0d5a3ef84152
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-21 12:51:19 +0530

    Time: 380 ms (17.15%) | Memory: 10.1 MB (89.77%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -5,17 +5,13 @@ public:
         if (sum & 1) return false;
 
         int targetSum = sum / 2;
-        std::vector<std::vector<bool>> dpVector(nums.size() + 1, std::vector<bool>(targetSum + 1, false));
-        for (int i = 0; i < dpVector.size(); i++)
-            dpVector[i][0] = true;
+        std::vector<bool> dpVector(targetSum + 1, false);    
+        dpVector[0] = true;
 
-        for (int j = 1; j < dpVector[0].size(); j++)
-            dpVector[0][j] = false;
-
-        for (int i = 1; i < dpVector.size(); i++)
-            for (int j = 1; j < dpVector[0].size(); j++)
-                dpVector[i][j] = dpVector[i-1][j] || (nums[i-1] <= j ? dpVector[i-1][j - nums[i-1]] : 0);
+        for (int i = 1; i <= nums.size(); i++)
+            for (int j = targetSum; j >= 0; j--)
+                dpVector[j] = dpVector[j] || (nums[i-1] <= j ? dpVector[j - nums[i-1]] : 0);
        
-        return dpVector[nums.size()][targetSum];
+        return dpVector[targetSum];
     }
 };
