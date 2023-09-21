78e60724549cc91fe3c500f44e6b09c3ee2ce0a2
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-21 12:44:21 +0530

    Time: 1118 ms (5.01%) | Memory: 13.4 MB (63.68%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        if (sum & 1) return false;
+
+        int targetSum = sum / 2;
+        std::vector<std::vector<bool>> dpVector(nums.size() + 1, std::vector<bool>(targetSum + 1, false));
+        for (int i = 0; i < dpVector.size(); i++)
+            dpVector[i][0] = true;
+
+        for (int j = 1; j < dpVector[0].size(); j++)
+            dpVector[0][j] = false;
+
+        for (int i = 1; i < dpVector.size(); i++) {
+            for (int j = 1; j < dpVector[0].size(); j++) {
+                dpVector[i][j] = dpVector[i][j] || dpVector[i-1][j];
+                if (nums[i-1] <= j) dpVector[i][j] = dpVector[i][j] || dpVector[i-1][j - nums[i-1]];          
+            }
+        }
+        
+        return dpVector[nums.size()][targetSum];
+    }
+};
