827fa0e17ba9acaa911e113371dd8c1b76960bb6
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-22 12:29:22 +0530

    Time: 810 ms (37.33%) | Memory: 8.3 MB (14.28%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,9 +1,9 @@
 class Solution {
-    bool existRecursive(std::vector<std::vector<char>> &board, std::string word, int x, int y, int xPrev, int yPrev, int currentIndex) {
+    bool existRecursive(std::vector<std::vector<char>> &board, std::string word, int x, int y, int currentIndex) {
         if (currentIndex == word.size())
             return true;
 
-        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || (x == xPrev && y == yPrev) || board[x][y] != word[currentIndex]) 
+        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y] != word[currentIndex]) 
             return false;
         
         char ch = board[x][y];
@@ -16,7 +16,7 @@ class Solution {
         for (int k = 0; k < 4; k++) {
             int xDash = x + dirx[k];
             int yDash = y + diry[k];
-            result = result || existRecursive(board, word, xDash, yDash, x, y, currentIndex + 1);
+            result = result || existRecursive(board, word, xDash, yDash, currentIndex + 1);
         }
 
         board[x][y] = ch;
@@ -30,7 +30,7 @@ public:
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++){
                 if (board[i][j] == word[0])
-                    result = result || existRecursive(board, word, i, j, -1, -1, 0);
+                    result = result || existRecursive(board, word, i, j, 0);
             }
         }
 
