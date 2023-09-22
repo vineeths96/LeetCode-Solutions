d81a754e5d6dcae5b9095a9e9985b7c078e61654
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-22 10:13:20 +0530

    Time: 17 ms (60.49%) | Memory: 23.4 MB (26.02%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    std::vector<std::vector<int>> dpVector;
+
+    int findTargetSumWaysRecursive(const std::vector<int> &nums, int target, int totalSum, int currentSum, int currentIndex) {
+        if (currentIndex == nums.size()) {
+            if (target == currentSum) return 1;
+            else return 0;
+        }
+        
+        if (currentSum - target + totalSum < 0) return 0;
+        if (dpVector[currentIndex][currentSum + totalSum] != -1) 
+            return dpVector[currentIndex][currentSum + totalSum];
+
+        int result = 0;
+        result += findTargetSumWaysRecursive(nums, target, totalSum, currentSum - nums[currentIndex], currentIndex + 1);
+        result += findTargetSumWaysRecursive(nums, target, totalSum, currentSum + nums[currentIndex], currentIndex + 1);
+        
+        return dpVector[currentIndex][currentSum + totalSum] = result;
+    }
+
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum = std::accumulate(nums.begin(), nums.end(), 0);        
+        dpVector = std::vector<std::vector<int>>(nums.size() + 1, std::vector<int>(2 * totalSum + 1, -1)); 
+        return findTargetSumWaysRecursive(nums, target, totalSum, 0, 0);
+    }
+};
