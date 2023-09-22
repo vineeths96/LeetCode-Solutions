7c0f64db7424eb6a932c2036538f19f92b51e07b
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-22 12:04:53 +0530

    Time: 844 ms (25.22%) | Memory: 8.3 MB (14.28%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    bool existRecursive(std::vector<std::vector<char>> &board, std::string word, int x, int y, int xPrev, int yPrev, int currentIndex) {
+        if (currentIndex == word.size())
+            return true;
+
+        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || (x == xPrev && y == yPrev) || board[x][y] != word[currentIndex]) 
+            return false;
+        
+        char ch = board[x][y];
+        board[x][y] = '|';
+
+        bool result = false;
+        int dirx[] = {0, 1, 0, -1};
+        int diry[] = {-1, 0, 1, 0};
+
+        for (int k = 0; k < 4; k++) {
+            int xDash = x + dirx[k];
+            int yDash = y + diry[k];
+            result = result || existRecursive(board, word, xDash, yDash, x, y, currentIndex + 1);
+        }
+
+        board[x][y] = ch;
+
+        return result;
+    }
+
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        bool result = false;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++){
+                if (board[i][j] == word[0])
+                    result = result || existRecursive(board, word, i, j, -1, -1, 0);
+            }
+        }
+
+        return result;
+    }
+};
