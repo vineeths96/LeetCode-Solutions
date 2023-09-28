87ce3abec644fceb88a155ced5cd8f1ae4753db2
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-09-28 15:46:37 +0530

    Time: 31 ms (52.63%) | Memory: 23.7 MB (26.26%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,19 @@
 class Solution {
-    std::vector<std::vector<int>> dpVector;
-
-    int findTargetSumWaysRecursive(const std::vector<int> &nums, int target, int totalSum, int currentSum, int currentIndex) {
-        if (currentIndex == nums.size()) {
-            if (target == currentSum) return 1;
-            else return 0;
-        }
-        
-        if (dpVector[currentIndex][currentSum + totalSum] != -1) 
-            return dpVector[currentIndex][currentSum + totalSum];
-
-        int result = 0;
-        result += findTargetSumWaysRecursive(nums, target, totalSum, currentSum - nums[currentIndex], currentIndex + 1);
-        result += findTargetSumWaysRecursive(nums, target, totalSum, currentSum + nums[currentIndex], currentIndex + 1);
-        
-        return dpVector[currentIndex][currentSum + totalSum] = result;
-    }
-
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = std::accumulate(nums.begin(), nums.end(), 0);        
-        dpVector = std::vector<std::vector<int>>(nums.size() + 1, std::vector<int>(2 * totalSum + 1, -1)); 
-        return findTargetSumWaysRecursive(nums, target, totalSum, 0, 0);
+        int totalSum = std::accumulate(nums.begin(), nums.end(), 0);
+        std::vector<std::vector<int>> dpVector(nums.size() + 1, std::vector<int>(2 * totalSum + 1, 0));
+
+        dpVector[0][totalSum] = 1;
+        for (int i = 1; i <= nums.size(); i++) {
+            for (int j = -totalSum; j <= totalSum; j++) {
+                if (dpVector[i - 1][j + totalSum] <= 0) continue;
+
+                dpVector[i][j + nums[i - 1] + totalSum] += dpVector[i - 1][j + totalSum];
+                dpVector[i][j - nums[i - 1] + totalSum] += dpVector[i - 1][j + totalSum];
+            }
+        }
+
+        return std::abs(target) > totalSum ? 0 : dpVector[nums.size()][target + totalSum];
     }
 };
