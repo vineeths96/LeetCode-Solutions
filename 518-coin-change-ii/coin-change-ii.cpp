efae6c751eff1fe038b02d8ccbfe8f43647a7c5d
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-10-08 11:08:07 +0530

    Time: 41 ms (16.30%) | Memory: 19.1 MB (17.66%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,27 +1,15 @@
 class Solution {
-    std::vector<std::vector<int>> dp;
-
-    int changeRecursive(std::vector<int> coins, int amount, int currentIndex) {
-        if (currentIndex == coins.size()) {
-            if (amount == 0) return 1;
-            else return 0;
-        }
-
-        if (dp[currentIndex][amount] != -1)
-            return dp[currentIndex][amount];
-
-        int includeCoinWays = 0, excludeCoinWays = 0;
-
-        excludeCoinWays = changeRecursive(coins, amount, currentIndex + 1);
-        if (coins[currentIndex] <= amount)
-            includeCoinWays = changeRecursive(coins, amount - coins[currentIndex], currentIndex);
-
-        return dp[currentIndex][amount] = includeCoinWays + excludeCoinWays;
-    }
-
 public:
     int change(int amount, vector<int>& coins) {
-        dp = std::vector<std::vector<int>>(coins.size() + 1, std::vector<int>(amount + 1, -1));
-        return changeRecursive(coins, amount, 0);        
+        std::vector<std::vector<int>> dpVector(coins.size() + 1, std::vector<int>(amount + 1, 0));
+
+        for (int i = 0; i < dpVector.size(); i++)
+            dpVector[i][0] = 1;
+
+        for (int i = 1; i < dpVector.size(); i++)
+            for (int a = 1; a < dpVector[0].size(); a++)
+                dpVector[i][a] = dpVector[i - 1][a] + (coins[i - 1] <= a ? dpVector[i][a - coins[i - 1]] : 0);
+
+        return dpVector[coins.size()][amount];
     }
 };
