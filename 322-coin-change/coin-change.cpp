59aadfaea4c57a5f52746e3475f2af88f489eceb
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-10-08 12:18:15 +0530

    Time: 148 ms (9.64%) | Memory: 49.5 MB (14.05%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,22 @@
 class Solution {
-    std::vector<std::vector<int>> dpVector;
-
-    int coinChangeRecursive(const std::vector<int> &coins, int amount, int currentIndex) {
-        if (currentIndex == coins.size()) {
-            if (amount == 0) return 0;
-            else return std::numeric_limits<int>::max();
-        }
-
-        if (dpVector[currentIndex][amount] != -1)
-            return dpVector[currentIndex][amount];
-
-        int includeCount = std::numeric_limits<int>::max(), excludeCount = std::numeric_limits<int>::max();
-        excludeCount = coinChangeRecursive(coins, amount, currentIndex + 1);
-        if (coins[currentIndex] <= amount) {
-            includeCount = coinChangeRecursive(coins, amount - coins[currentIndex], currentIndex);
-            if (includeCount != std::numeric_limits<int>::max()) includeCount += 1;
-        }
-        
-        return dpVector[currentIndex][amount] = std::min(includeCount, excludeCount);
-    }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
-        dpVector = std::vector<std::vector<int>>(coins.size() + 1, std::vector<int>(amount + 1, -1));
-        int minCoins = coinChangeRecursive(coins, amount, 0);
-        return minCoins == std::numeric_limits<int>::max() ? -1 : minCoins;
+        std::vector<std::vector<int>> dpVector(coins.size() + 1, std::vector<int>(amount + 1, std::numeric_limits<int>::max()));
+
+        for (int i = 0; i < dpVector.size(); i++)
+            dpVector[i][0] = 0;
+
+        for (int i = 1; i < dpVector.size(); i++) {
+            for (int a = 1; a < dpVector[0].size(); a++) {
+                int excludeCount = dpVector[i - 1][a];
+                int includeCount = std::numeric_limits<int>::max();
+                if (coins[i - 1] <= a && dpVector[i][a - coins[i -1]] != std::numeric_limits<int>::max())
+                    includeCount = 1 + dpVector[i][a - coins[i -1]];
+
+                dpVector[i][a] = std::min(includeCount, excludeCount);
+            }
+        }
+
+        return dpVector[coins.size()][amount] == std::numeric_limits<int>::max() ? -1 : dpVector[coins.size()][amount];
     }
 };
