9c8c1026c9f7f86149877c413af7f7e14c398b61
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-10-08 11:11:09 +0530

    Time: 37 ms (23.81%) | Memory: 8 MB (69.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        std::vector<std::vector<int>> dpVector(coins.size() + 1, std::vector<int>(amount + 1, 0));
+        std::vector<std::vector<int>> dpVector(2, std::vector<int>(amount + 1, 0));
 
         for (int i = 0; i < dpVector.size(); i++)
             dpVector[i][0] = 1;
 
-        for (int i = 1; i < dpVector.size(); i++)
-            for (int a = 1; a < dpVector[0].size(); a++)
-                dpVector[i][a] = dpVector[i - 1][a] + (coins[i - 1] <= a ? dpVector[i][a - coins[i - 1]] : 0);
+        for (int i = 1; i <= coins.size(); i++)
+            for (int a = 1; a <= amount; a++)
+                dpVector[i % 2][a] = dpVector[(i - 1) % 2][a] + (coins[i - 1] <= a ? dpVector[i % 2][a - coins[i - 1]] : 0);
 
-        return dpVector[coins.size()][amount];
+        return dpVector[coins.size() % 2][amount];
     }
 };
