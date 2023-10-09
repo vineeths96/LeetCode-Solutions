ba46db61cfcc34a87a6a07de69a50d770bb489fc
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-10-09 09:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (16.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,8 +2,14 @@ class Solution {
 public:
     int fib(int n) {
         if (n == 0) return 0;
-        else if (n == 1) return 1;
 
-        return fib(n  - 1) + fib(n - 2);
+        std::vector<int> dpVector(n + 1, -1);
+        dpVector[0] = 0;
+        dpVector[1] = 1;
+
+        for (int i = 2; i < dpVector.size(); i++)
+            dpVector[i] = dpVector[i - 1] + dpVector[i - 2];
+
+        return dpVector[n];
     }
 };
