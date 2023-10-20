6f12c7237ff9062f3b32aee882db8836a02daf9b
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-10-20 17:40:20 +0530

    Time: 452 ms (6.64%) | Memory: 17.5 MB (19.99%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,29 +1,16 @@
 class Solution {
-    std::vector<int> dpVector;
-
-    int jumpRecursive(const std::vector<int> &nums, int currentIndex) {
-        if (currentIndex == nums.size() - 1)
-            return 0;
-
-        if (dpVector[currentIndex] != -1)
-            return dpVector[currentIndex];
-
-        int maxSteps = nums[currentIndex];
-        int minStepsToEnd = std::numeric_limits<int>::max();
-        for (int step = 1; step <= maxSteps; step++) {
-            if (currentIndex + step >= nums.size()) break;
-
-            int nextResult = jumpRecursive(nums, currentIndex + step);
-            if (nextResult != std::numeric_limits<int>::max()) 
-                minStepsToEnd = std::min(minStepsToEnd, 1 + nextResult);
-        }
-
-        return dpVector[currentIndex] = minStepsToEnd;
-    } 
-
 public:
     int jump(vector<int>& nums) {
-        dpVector = std::vector<int>(nums.size(), -1);
-        return jumpRecursive(nums, 0);
+        std::vector<int> dpVector(nums.size(), std::numeric_limits<int>::max());
+        dpVector.back() = 0;
+
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            for (int step = 1; step <= nums[i]; step++) {
+                if (i + step < nums.size() && dpVector[i + step] != std::numeric_limits<int>::max())
+                    dpVector[i] = std::min(dpVector[i], 1 + dpVector[i + step]);
+            }
+        }
+
+        return dpVector[0];
     }
 };
