38dd579e25034c204819f7c47096eb13e8f8927f
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-10-21 20:12:23 +0530

    Time: 40 ms (20.67%) | Memory: 8 MB (60.46%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,31 +1,23 @@
 class Solution {
-    int count;
-    std::vector<std::vector<int>> dpVector;
-
-    bool countSubstringsRecursive(const std::string &str, int start, int end) {
-        if (start > end)
-            return true;
-        
-        if (dpVector[start][end] != -1)
-            return dpVector[start][end] == 1;
-
-        bool isPalindrome = false;
-        if (str[start] == str[end]) {
-            isPalindrome = countSubstringsRecursive(str, start + 1, end - 1);
-            if (isPalindrome) count++;
-        }
-
-        countSubstringsRecursive(str, start + 1, end);
-        countSubstringsRecursive(str, start, end - 1);
-
-        return dpVector[start][end] = isPalindrome ? 1 : 0;
-    }
-
 public:
     int countSubstrings(string s) {
-        count = 0;
-        dpVector = std::vector<std::vector<int>>(s.size(), std::vector<int>(s.size(), -1));
-        countSubstringsRecursive(s, 0, s.size() - 1);
+        int count = 0;
+        std::vector<std::vector<bool>> dpVector(s.size(), std::vector<bool>(s.size(), false));
+
+        for (int i = 0; i < dpVector.size(); i++)
+            for (int j = 0; j <= i; j++)
+                dpVector[i][j] = true;
+
+        count += dpVector.size();
+        for (int start = dpVector.size() - 1; start >= 0; start--) {
+            for (int end = start + 1; end < dpVector[0].size(); end++) {
+                if (s[start] == s[end] && dpVector[start + 1][end - 1]) {
+                    dpVector[start][end] = true;
+                    count++;
+                }
+            }
+        }
+
         return count;
     }
 };
