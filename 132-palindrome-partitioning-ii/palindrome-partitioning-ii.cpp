c693fa945c79b395034257c869b98e03faef4786
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-01 08:20:34 +0530

    Time: 1519 ms (25.69%) | Memory: 7 MB (53.74%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,34 +1,34 @@
 class Solution {
-    std::vector<std::vector<int>> dpVector;
+    std::vector<int> dpVector;
 
-    bool isPalindrome(const std::string &str, int left, int right) {
-        while (left < right) {
-            if (str[left++] != str[right--])
+    bool isPalindrome(const std::string &str, int start, int end) {
+        while (start < end) {
+            if (str[start++] != str[end--])
                 return false;
         }
 
         return true;
     }
 
-    int minCutRecursive(const std::string &str, int start, int end) {
-        if (start > end)
+    int minCutRecursive(const std::string &str, int currentIndex) {
+        if (currentIndex >= str.size())
             return 0;
 
-        if (dpVector[start][end] != -1)
-            return dpVector[start][end];
+        if (dpVector[currentIndex] != -1)
+            return dpVector[currentIndex];
 
-        int minCuts = end - start + 1;
-        for (int ind = start; ind <= end; ind++) {
-            if (isPalindrome(str, start, ind))
-                minCuts = std::min(minCuts, (ind == end ? 0 : 1) + minCutRecursive(str, ind + 1, end));
+        int minCuts = str.size() - currentIndex;
+        for (int ind = currentIndex; ind < str.size(); ind++) {
+            if (isPalindrome(str, currentIndex, ind))
+                minCuts = std::min(minCuts, (ind == str.size() - 1 ? 0 : 1) + minCutRecursive(str, ind + 1));
         }
 
-        return dpVector[start][end] = minCuts;
+        return dpVector[currentIndex] = minCuts;
     }
 
 public:
     int minCut(string s) {
-        dpVector = std::vector<std::vector<int>>(s.size(), std::vector<int>(s.size(), -1));
-        return minCutRecursive(s, 0, s.size() - 1);
+        dpVector = std::vector<int>(s.size(), -1);
+        return minCutRecursive(s, 0);
     }
 };
