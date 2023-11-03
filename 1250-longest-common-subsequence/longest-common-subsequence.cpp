ba7fcff2dd101a775d5b546d3aa8c5c870ae78d4
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-03 08:08:24 +0530

    Time: 48 ms (62.56%) | Memory: 6.8 MB (92.36%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        std::vector<std::vector<int>> dpVector(text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+        std::vector<std::vector<int>> dpVector(2, std::vector<int>(text2.size() + 1, 0));
 
-        for (int i = 1; i < dpVector.size(); i++) {
-            for (int j = 1; j < dpVector[0].size(); j++) {
+        for (int i = 1; i <= text1.size(); i++) {
+            for (int j = 1; j <= text2.size(); j++) {
                 if (text1[i - 1] == text2[j - 1])
-                    dpVector[i][j] = 1 + dpVector[i - 1][j - 1];
+                    dpVector[i % 2][j] = 1 + dpVector[(i - 1) % 2][j - 1];
                 else
-                    dpVector[i][j] = std::max(dpVector[i - 1][j], dpVector[i][j - 1]);
+                    dpVector[i % 2][j] = std::max(dpVector[(i - 1) % 2][j], dpVector[i % 2][j - 1]);
             }
         }
         
-        return dpVector[text1.size()][text2.size()];
+        return dpVector[text1.size() % 2][text2.size()];
     }
 };
