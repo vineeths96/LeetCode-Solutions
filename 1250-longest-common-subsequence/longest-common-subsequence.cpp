d1cd706d7fbeafaea3aa9f8a38f3de7f7d66e492
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-03 08:05:42 +0530

    Time: 58 ms (29.83%) | Memory: 24.9 MB (17.36%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,17 @@
 class Solution {
-    std::vector<std::vector<int>> dpVector;
-
-    int longestCommonSubsequenceRecursive(const std::string &text1, const std::string &text2, int index1, int index2) {
-        if (index1 == text1.size() || index2 == text2.size())
-            return 0;
-
-        if (dpVector[index1][index2] != -1)
-            return dpVector[index1][index2];
-
-        int result = 0;
-        if (text1[index1] == text2[index2]) 
-            result = 1 + longestCommonSubsequenceRecursive(text1, text2, index1 + 1, index2 + 1);
-        else
-            result = std::max(longestCommonSubsequenceRecursive(text1, text2, index1 + 1, index2), longestCommonSubsequenceRecursive(text1, text2, index1, index2 + 1));
-
-        return dpVector[index1][index2] = result;
-    }
-
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        dpVector = std::vector<std::vector<int>>(text1.size() + 1, std::vector<int>(text2.size() + 1, -1));
-        return longestCommonSubsequenceRecursive(text1, text2, 0, 0);
+        std::vector<std::vector<int>> dpVector(text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+
+        for (int i = 1; i < dpVector.size(); i++) {
+            for (int j = 1; j < dpVector[0].size(); j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    dpVector[i][j] = 1 + dpVector[i - 1][j - 1];
+                else
+                    dpVector[i][j] = std::max(dpVector[i - 1][j], dpVector[i][j - 1]);
+            }
+        }
+        
+        return dpVector[text1.size()][text2.size()];
     }
 };
