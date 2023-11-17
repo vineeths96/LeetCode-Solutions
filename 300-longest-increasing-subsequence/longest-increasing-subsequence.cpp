ae1dd287002385edc8108b0c38c384ff3e70eadf
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-17 08:50:16 +0530

    Time: 3 ms (98.80%) | Memory: 10.8 MB (62.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        std::vector<int> dpVector(nums.size() , 1);
-        
-        int maxLength = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j])
-                    dpVector[i] = std::max(dpVector[i], 1 + dpVector[j]);
-                
-                maxLength = std::max(maxLength, dpVector[i]);
+        std::vector<int> subSequence;
+
+        for (const int &num : nums) {
+            if (subSequence.empty() || subSequence.back() < num)
+                subSequence.push_back(num);
+            else {
+                int ind = std::lower_bound(subSequence.begin(), subSequence.end(), num) - subSequence.begin();
+                subSequence[ind] = num;
             }
         }
-        return maxLength;
+        
+        return subSequence.size();
     }
 };
