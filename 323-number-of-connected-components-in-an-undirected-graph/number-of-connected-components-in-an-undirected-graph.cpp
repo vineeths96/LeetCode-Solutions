279dd28c6da886e3496e4eae1354bf1a13be6b95
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-23 08:24:59 +0530

    Time: 8 ms (97.38%) | Memory: 12.3 MB (94.76%) - LeetSync

---
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    class DSU {
+        std::vector<int> rank;
+        std::vector<int> parent;
+
+    public:
+        DSU(int n) {
+            rank = std::vector<int>(n, 1);
+            parent = std::vector<int>(n, 0);
+            for (int i = 0; i < n; i++)
+                parent[i] = i;
+        }
+
+        int find(int node) {
+            if (parent[node] != node)
+                parent[node] = find(parent[node]);
+
+            return parent[node];
+        }
+
+        bool unionSet(int nodeX, int nodeY) {
+            int groupX = find(nodeX);
+            int groupY = find(nodeY);
+
+            if (groupX == groupY)
+                return false;
+
+            if (rank[groupX] < rank[groupY])
+                parent[groupX] = groupY;
+            else if (rank[groupX] > rank[groupY])
+                parent[groupY] = groupX;
+            else {
+                rank[groupX]++;
+                parent[groupY] = groupX;
+            }
+
+            return true;
+        }
+    };
+
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        DSU unionFind = DSU(n);
+
+        int numComponents = n;
+        for (const auto &edge : edges)
+            if (unionFind.unionSet(edge[0], edge[1]))
+                numComponents--;
+        
+        return numComponents;
+    }
+};
