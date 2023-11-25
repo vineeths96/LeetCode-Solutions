5d1ad17225e2e8e31b496ce495bfc8396d81b13d
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-25 14:36:48 +0530

    Time: 7 ms (98.99%) | Memory: 18.4 MB (71.97%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        std::set<std::pair<int, char>> rowSet;
-        std::set<std::pair<int, char>> colSet;
-        std::set<std::pair<int, char>> subBoxSet;
+        int N = 9;
+        
+        std::vector<int> rows(N, 0);
+        std::vector<int> cols(N, 0);
+        std::vector<int> boxes(N, 0);
 
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 if (board[i][j] == '.')
                     continue;
 
-                bool insertSuccess = true;
-                insertSuccess = insertSuccess && rowSet.insert({i, board[i][j]}).second;
-                insertSuccess = insertSuccess && colSet.insert({j, board[i][j]}).second;
+                int num = board[i][j] - '0';
+                int mask = 1 << (num - 1);
 
-                int rowStartInd = (i / 3) * 3;
-                int colStartInd = (j / 3) * 3;
-                insertSuccess = insertSuccess && subBoxSet.insert({rowStartInd * 10 + colStartInd, board[i][j]}).second;
+                if (rows[i] & mask) return false;
+                else rows[i] |= mask;
 
-                if (!insertSuccess)
-                    return false;
+                if (cols[j] & mask) return false;
+                else cols[j] |= mask;
+
+                int boxNumber = (i / 3) * 3 + j / 3;
+                if (boxes[boxNumber] & mask) return false;
+                else boxes[boxNumber] |= mask;
             }
         }
 
