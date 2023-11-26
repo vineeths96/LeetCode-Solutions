67b3f5587c61e76f8ea39cd77ad7ed3ae2293c65
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-11-26 12:17:21 +0530

    Time: 2 ms (88.33%) | Memory: 10.3 MB (53.30%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,15 +1,36 @@
 class Solution {
+    struct TrieNode {
+        bool isWord;
+        std::unordered_map<char, TrieNode*> children;
+        TrieNode() : isWord(false), children(std::unordered_map<char, TrieNode*>()) {} 
+    };
+
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        TrieNode *root = new TrieNode();
+        for (const auto &word : wordDict) {
+            TrieNode *current = root;
+            for (const char &ch : word) {
+                if (current->children.find(ch) == current->children.end())
+                    current->children[ch] = new TrieNode();
+                
+                current = current->children[ch];
+            }
+
+            current->isWord = true;
+        } 
+
         std::vector<bool> dpVector(s.size() + 1, false);
 
         dpVector[0] = true;
         for (int i = 1; i < dpVector.size(); i++) {
-            for (const auto &word : wordDict) {
-                if (i < word.size())
-                    continue;
-
-                dpVector[i] = dpVector[i] || ((s.substr(i - word.size(), word.size()) == word) && dpVector[i - word.size()]);
+            TrieNode *current = root;
+            for (int j = i; j < dpVector.size(); j++) {
+                if (current->children.find(s[j - 1]) == current->children.end())
+                    break;
+                
+                current = current->children[s[j - 1]];
+                dpVector[j] = dpVector[j] || (dpVector[i - 1] && current->isWord);
             }
         }
 
