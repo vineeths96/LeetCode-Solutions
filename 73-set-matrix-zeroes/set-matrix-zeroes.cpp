ca413795965444af711033913e5cdcb001d5b9a1
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-12-09 17:24:18 +0530

    Time: 12 ms (56.38%) | Memory: 13.8 MB (76.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,20 +4,37 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        std::vector<std::pair<int, int>> zeroes;
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                if (matrix[i][j] == 0)
-                    zeroes.push_back({i, j});
+        bool firstRow = false;
+        bool firstCol = false;
 
-        for (const auto &zero : zeroes) {
-            auto [x, y] = zero;
-            
-            for (int j = 0; j < cols; j++)
-                matrix[x][j] = 0;
-                
+        for (int i = 0; i < rows; i++)
+            firstCol = firstCol || matrix[i][0] == 0;
+
+        for (int j = 0; j < cols; j++)
+            firstRow = firstRow || matrix[0][j] == 0;
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if (firstCol) {
             for (int i = 0; i < rows; i++)
-                matrix[i][y] = 0;                
+                matrix[i][0] = 0;
+        }
+
+        if (firstRow) {
+            for (int j = 0; j < cols; j++)
+                matrix[0][j] = 0;
         }
     }
 };
