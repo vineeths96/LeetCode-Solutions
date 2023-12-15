2b10303d51c4c220d261027ddb88057c4cb20d51
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2023-12-15 15:06:26 +0530

    Time: 3 ms (77.27%) | Memory: 6.9 MB (63.83%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -3,24 +3,19 @@ public:
     string getHint(string secret, string guess) {
         int bulls = 0, cows = 0;
 
-        std::unordered_map<char, int> secretMap;
+        std::unordered_map<char, int> stringMap;
         for (int i = 0; i < secret.size(); i++) {
             if (secret[i] == guess[i])
                 bulls++;
-            else
-                secretMap[secret[i]]++;
-        }
-
-        for (int j = 0; j < guess.size(); j++) {
-            if (secret[j] == guess[j] || !secretMap.contains(guess[j]))
-                continue;
-            
-            if (secretMap[guess[j]]--) {
-                cows++;
-                if(secretMap[guess[j]] == 0) secretMap.erase(guess[j]);
+            else {
+                if (stringMap[secret[i]] < 0) cows++;
+                if (stringMap[guess[i]] > 0) cows++;
+                
+                stringMap[secret[i]]++;
+                stringMap[guess[i]]--;
             }
         }
-
+        
         return std::to_string(bulls) + "A" + std::to_string(cows) + "B"; 
     }
 };
