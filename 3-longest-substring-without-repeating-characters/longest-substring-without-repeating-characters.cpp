8a092d1bbded7119eba144aeb31d2388a6169c11
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-01 18:55:58 +0530

    Time: 29 ms (19.83%) | Memory: 8.7 MB (61.86%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int maxLength = 0;
-        std::unordered_map<char, int> charFreqMap;
+        std::unordered_map<int, int> charIndexMap;
 
         for (int windowStart = 0, windowEnd = 0; windowEnd < s.size(); windowEnd++) {
-            charFreqMap[s[windowEnd]]++;
-
-            while (windowStart <= windowEnd && charFreqMap[s[windowEnd]] > 1) {
-                charFreqMap[s[windowStart]]--;
-                windowStart++;
-            }
+            if (charIndexMap.contains(s[windowEnd]))
+                windowStart = std::max(windowStart, charIndexMap[s[windowEnd]] + 1);
 
             maxLength = std::max(maxLength, windowEnd - windowStart + 1);
+            charIndexMap[s[windowEnd]] = windowEnd;
         }
 
         return maxLength;
