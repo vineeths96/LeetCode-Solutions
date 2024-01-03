701565a004cf5bd18912f26da4dbdedb5af7f7ba
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-03 08:15:19 +0530

    Time: 311 ms (89.68%) | Memory: 15.7 MB (75.23%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -1,40 +1,42 @@
 class Solution {
     struct TrieNode {
-        bool isWord = false;
-        std::unordered_map<char, TrieNode*> charMap;
+        std::string isWord;
+        std::unordered_map<char, TrieNode*> children;
 
-        TrieNode() : isWord(false), charMap(std::unordered_map<char, TrieNode*>()) {};
+        TrieNode() : isWord(""), children(std::unordered_map<char, TrieNode*>()) {};
     };
     
-    void findWordsHelper(std::vector<std::vector<char>> &board, TrieNode *root, std::string &currentWord, int x, int y) {
+    void findWordsHelper(std::vector<std::vector<char>> &board, TrieNode *root, int x, int y) {
         if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size())
             return;
 
-        if (!root->charMap.contains(board[x][y]))
+        if (!root->children.contains(board[x][y]))
             return;
 
         char holder = board[x][y];
         board[x][y] = '#';
 
-        root = root->charMap[holder];
-        currentWord.push_back(holder);
-
-        if (root->isWord)
-            allWords.insert(currentWord);
+        TrieNode *child = root->children[holder];
+        if (!child->isWord.empty()) {
+            allWords.push_back(child->isWord);
+            child->isWord = "";
+        }
 
         for (const auto &dir : dirs) {
             int xDash = x + dir[0];
             int yDash = y + dir[1];
 
-            findWordsHelper(board, root, currentWord, xDash, yDash);
+            findWordsHelper(board, child, xDash, yDash);
         }
 
-        currentWord.pop_back();
+        if (child->children.empty())
+            root->children.erase(holder);
+
         board[x][y] = holder;
     }
 
     TrieNode *root;
-    std::unordered_set<std::string> allWords;
+    std::vector<std::string> allWords;
     std::vector<std::vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
 public:
@@ -44,20 +46,19 @@ public:
         for (const auto &word : words) {
             TrieNode *current = root;
             for (const auto &ch : word) {
-                if (!current->charMap.contains(ch))
-                    current->charMap[ch] = new TrieNode();
+                if (!current->children.contains(ch))
+                    current->children[ch] = new TrieNode();
 
-                current = current->charMap[ch];
+                current = current->children[ch];
             }
 
-            current->isWord = true;
+            current->isWord = word;
         }
 
-        std::string currentWord;
         for (int i = 0; i < board.size(); i++)
             for (int j = 0; j < board[0].size(); j++)
-                findWordsHelper(board, root, currentWord, i, j);
+                findWordsHelper(board, root, i, j);
 
-        return std::vector<std::string>(allWords.begin(), allWords.end());
+        return allWords;
     }
 };
