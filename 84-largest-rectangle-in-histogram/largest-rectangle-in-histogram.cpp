20d4b9ad0830f2e6ba1aac003e6a6750aad735a2
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-04 15:34:33 +0530

    Time: 121 ms (82.27%) | Memory: 77.6 MB (73.83%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        std::stack<int> monotonicStack;
+        monotonicStack.push(-1);
+
+        int area = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            while (monotonicStack.top() != -1 && heights[monotonicStack.top()] > heights[i]) {
+                int top = monotonicStack.top();
+                monotonicStack.pop();
+
+                int currentArea = 0;
+                if (monotonicStack.empty())
+                    currentArea = heights[top] * i;
+                else
+                    currentArea = heights[top] * (i - monotonicStack.top() - 1);
+
+                area = std::max(area, currentArea);
+            }
+
+            monotonicStack.push(i);
+        }
+
+        int lastIndex = monotonicStack.top();
+        while (monotonicStack.top() != -1) {
+            int top = monotonicStack.top();
+            monotonicStack.pop();
+
+            int currentArea = heights[top] * (lastIndex - monotonicStack.top()); 
+            area = std::max(area, currentArea);
+        }
+
+        return area;
+    }
+};
