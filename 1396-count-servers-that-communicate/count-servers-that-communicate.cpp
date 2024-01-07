7bcf54aad5c13c26afb0502fef886aa76a5cb977
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-07 08:52:22 +0530

    Time: 34 ms (97.13%) | Memory: 22.7 MB (54.83%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -4,23 +4,26 @@ public:
         int rows = grid.size();
         int cols = grid[0].size();
 
+        std::vector<int> rowCounts(rows, 0);
+        std::vector<int> colCounts(cols, 0);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 0)
+                    continue;
+
+                rowCounts[i]++;
+                colCounts[j]++;
+            }
+        }
+
         int servers = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 0)
                     continue;
 
-                int serversInCol = 0;
-                for (int iDash = 0; iDash < rows; iDash++) 
-                    if (grid[iDash][j] == 1) 
-                        serversInCol++;
-                
-                int serversInRow = 0;
-                for (int jDash = 0; jDash < cols; jDash++)
-                    if (grid[i][jDash] == 1)
-                        serversInRow++;
-
-                if (serversInRow > 1 || serversInCol > 1)
+                if (rowCounts[i] > 1 || colCounts[j] > 1)
                     servers++;
             }
         }
