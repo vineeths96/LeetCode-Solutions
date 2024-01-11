cfaf334433d5330233502cad08a62ade77301985
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-11 12:21:47 +0530

    Time: 10 ms (74.29%) | Memory: 20.2 MB (60.37%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        std::vector<int> leftMax(height.size(), -1);
-        std::vector<int> rightMax(height.size(),-1);
-
-        for (int i = 1; i < height.size(); i++)
-            leftMax[i] = std::max(leftMax[i - 1], height[i - 1]);
-        
-        for (int i = height.size() - 2; i >= 0; i--)
-            rightMax[i] = std::max(rightMax[i + 1], height[i + 1]);
-
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
         int water = 0;
-        for (int i = 0; i < height.size(); i++) {
-            int waterHeight = std::min(leftMax[i], rightMax[i]) - height[i];
-            water += std::max(waterHeight, 0);
+
+        while(left < right) {
+            if (height[left] < height[right]) {
+                leftMax = std::max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = std::max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;    
+            }
         }
 
         return water;
