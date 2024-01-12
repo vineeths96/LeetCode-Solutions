e35be198340a57d46f340b5702c85695c66c7763
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-12 14:44:26 +0530

    Time: 260 ms (79.32%) | Memory: 10.6 MB (57.66%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,29 +1,24 @@
 class Solution {
-    std::vector<std::vector<int>> dp;
-
-    int maxCoinsRecursive(const std::vector<int> &nums, int left, int right) {
-        if (left > right)
-            return 0;
-
-        if (dp[left][right] != -1)
-            return dp[left][right];
-
-        int maxCoins = 0;
-        for (int i = left; i <= right; i++) {
-            int coinsExceptI = maxCoinsRecursive(nums, left, i - 1) + maxCoinsRecursive(nums, i + 1, right);
-            int burstI = nums[left - 1] * nums[i] * nums[right + 1];
-            maxCoins = std::max(maxCoins, coinsExceptI + burstI);
-        }
-
-        return dp[left][right] = maxCoins;
-    }
-
 public:
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(), 1);
         nums.insert(nums.end(), 1);
 
-        dp = std::vector<std::vector<int>>(nums.size(), std::vector<int>(nums.size(), -1));
-        return maxCoinsRecursive(nums, 1, nums.size() - 2);
+        std::vector<std::vector<int>> dp(nums.size(), std::vector<int>(nums.size(), 0));
+        for (int left = dp.size() - 2; left > 0; left--) {
+            for (int right = left; right < dp[0].size() - 1; right++) {
+                int maxCoins = 0;
+                for (int i = left; i <= right; i++) {
+                    int exceptI = dp[left][i - 1] + dp[i + 1][right];
+                    int burstI = nums[left - 1] * nums[i] * nums[right + 1];
+
+                    maxCoins = std::max(maxCoins, exceptI + burstI);
+                }
+
+                dp[left][right] = maxCoins;
+            }
+        }
+
+        return dp[1][dp.size() - 2];
     }
 };
