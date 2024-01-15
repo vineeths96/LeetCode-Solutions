a27a71e9f2dda4bff3ae9de3460b5546907228b6
Vineeth S <50873201+vineeths96@users.noreply.github.com>
2024-01-15 08:24:21 +0530

    Time: 6 ms (84.73%) | Memory: 8.6 MB (82.90%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.cpp
+++ 482-license-key-formatting/license-key-formatting.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        std::stack<char> charStack;
-
-        for (const char &ch : s)
-            if (std::isdigit(ch) || std::isalpha(ch))
-                charStack.push(std::toupper(ch));
+        std::string formattedString;
 
         int charsInGroup = 0;
-        std::string formattedString;
-        while (!charStack.empty()) {
-            char ch = charStack.top();
-            charStack.pop();
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '-')
+                continue;
 
-            formattedString.push_back(ch);
+            formattedString.push_back(std::toupper(s[i]));
             if (++charsInGroup % k == 0)
                 formattedString.push_back('-');
         }
 
-        if (!formattedString.empty() && formattedString.back() == '-') 
+        if (!formattedString.empty() && formattedString.back() == '-')
             formattedString.pop_back();
-        
+
         std::reverse(formattedString.begin(), formattedString.end());
         return formattedString;
     }
